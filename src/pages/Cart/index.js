d03d8dbe3fd4104dbc8efb49a045489a9f02cc6c
Roger <12768403+rogix@users.noreply.github.com>
2019-07-29 15:00:28 -0300

    chore: add style to cart component

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,68 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/28/COL-3586-128/COL-3586-128_detalhe2.jpg?resize=326:*"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis muito massa</strong>
+              <span>159,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$319,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7209c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finlizar Pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1925,35</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
